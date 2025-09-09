7dc02ffab8fe6867ea15ab359b48c891b10947c0
jesus0121 <jrodriguez884@unab.edu.co>
2025-09-09 14:17:44 -0500

    Controlador Basico

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function index()
+    {
+        return 'List products';
+    }
+
+    function detail($id, $category = null)
+    {
+        if ($category != null) {
+
+            return 'Detail products: ' . $id . ". With category: " . $category;
+        } else {
+            return 'Detail products: ' . $id;
+    }
+
+    function create(){
+            return "FORM FOR CREATE PRODUCTS";
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function () {
-    return view('List products');
-});
+Route::get('/products', [ProductController::class, 'index'] );
 
-Route::get('/products/{id}/{category?}', function ($id, $category = null) {
-    if ($category != null){
-        
-        return 'Detail products: ' . $id . ". With category: ". $category;
-
-    }else {
-        return 'Detail products: ' . $id;
-    }    
-});
-
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::get('/products/{id}/{category?}', [ProductController::class, 'detail']);
 
