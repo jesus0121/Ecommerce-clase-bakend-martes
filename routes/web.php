c7c5bd027ad7543471e66836e6714b99bebcbefc
jesus0121 <jrodriguez884@unab.edu.co>
2025-09-09 13:51:53 -0500

    Rutas Basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('List products');
+});
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+    if ($category != null){
+        
+        return 'Detail products: ' . $id . ". With category: ". $category;
+
+    }else {
+        return 'Detail products: ' . $id;
+    }    
+});
+
+
