0e05f28aff5a3e3f343c27b60633880008a7423d
jesus0121 <jrodriguez884@unab.edu.co>
2025-09-09 15:46:26 -0500

    layouts en laravel

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecommerce</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Product Detail {{ $id }}</h1>
     <h2>Con category {{ $category }}</h2>
-</body>
-</html>
+@endsection
