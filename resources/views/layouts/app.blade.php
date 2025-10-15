01a0b52b526b3892cf0e13aa367149fb50dd4385
jesus0121 <jrodriguez884@unab.edu.co>
2025-10-15 14:48:03 -0500

    Se configuro el login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,11 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    
 
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-   
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
     <div id="app">
