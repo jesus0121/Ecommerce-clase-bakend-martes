37a2c2c9ee79154964bcb10187d493cd6be86f31
jesus0121 <jrodriguez884@unab.edu.co>
2025-10-28 15:17:05 -0500

    create category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function create(){
         return view("admin.categories.create");
+    }
+
+    public function store(Request $request){
+
+        Category::create([
+            'name' => $request->get('name')
+        ]);
+        return "Se guardo XDXDXD";
     }
 }
